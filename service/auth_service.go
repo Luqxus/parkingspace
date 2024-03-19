4255cd6aba309b42a9f417d16ee9050beb03078a
luqxus <lebelwane@protonmain.com>
2024-03-19 20:40:15 +0200

    space & authentication services structure

---
+++ service/auth_service.go
@@ -0,0 +1,30 @@
+package service
+
+
+type AuthService interface {}
+
+type authService struct {}
+
+
+func NewAuthService() *authService {
+	return &authService{}
+}
+
+
+func (s *authService) CreateUser() {
+//	TODO: create a new user
+}
+
+func (s *authService) LoginUser () {
+//	TODO: login user logic
+}
+
+
+func (s *authService) DeleteUser() {
+//	TODO: delete user account logic
+}
+
+func (s *authService) UpdateUser() {
+//	TODO: update user logic
+}
+
---
+++ service/space_service.go
@@ -0,0 +1,27 @@
+package service
+
+type SpaceService interface {}
+
+type spaceService struct {}
+
+
+func NewSpaceService() *spaceService {
+	return &spaceService{}
+}
+
+func GetNearestSpaces() {
+//	TODO: parking spaces in my radius
+}
+
+func GetSpace() {
+//	TODO: get parking space by id
+}
+
+
+func GetSpaceInLocation() {
+//	TODO: get parking space in specific location
+}
+
+func GetFilteredSpaces() {
+//	TODO: get parking spaces that fit a filter
+}
